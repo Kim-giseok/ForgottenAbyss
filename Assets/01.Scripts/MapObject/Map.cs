82feaada06f7a056b38f2d517332d54193cc0658
Kim-giseok <belbeck@naver.com>
2025-04-08 11:28:18 +0900

    Feat: 맵 시작시 플레이어 위치 초기화
    맵이 새로 생성되면 플레이어의 위치를 시작지점으로 초기화합니다.

--- Assets/01.Scripts/MapObject/Map.cs
+++ Assets/01.Scripts/MapObject/Map.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,5 +10,13 @@ public class Map : MonoBehaviour
     public void MapStart()
     {
         //move player's transform to startP
+        try
+        {
+            GameManager.Instance.player.transform.position = startP.position;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
     }
 }
