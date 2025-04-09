1b1bdba634a609de4cdbd4189f064c456b22c643
Kim-giseok <belbeck@naver.com>
2025-04-09 19:47:51 +0900

    Fix: 머지 후 적 전부 처치 동작 수정
    머지 후 몬스터 메니저에 적을 추가/삭제하는 코드를 재삽입했습니다.
    적 공격이 가능하도록 레이어를 추가, 플레이어와 적의 레이어를 분리했습니다.

--- Assets/01.Scripts/Battle/Enemies/Controllers/EnemyController.cs
+++ Assets/01.Scripts/Battle/Enemies/Controllers/EnemyController.cs
@@ -7,17 +7,17 @@ public class EnemyController : MonoBehaviour, IDamagable
     [Header("Resource")]
     public float health;
     public float attack;
-    
+
     public Rigidbody2D rigidbody { get; private set; }
     public BTMachine btMachine { get; private set; }
     public EnemyAgent agent { get; private set; }
     public EnemyAnimationHandler animationHandler { get; private set; }
     public EnemyStatusHandler statusHandler { get; private set; }
     public EnemyRewardHandler rewardHandler { get; private set; }
-    
+
     public RespawnArea respawnArea { get; private set; }
-    
-    
+
+
     private Transform pivot;
     public GameObject currWeapon; // 무기의 애니메이션이 발생할 수도 있음
 
@@ -32,21 +32,31 @@ public class EnemyController : MonoBehaviour, IDamagable
         btMachine = new(this);
     }
 
-    public virtual void Start() { }
+    public virtual void Start()
+    {
+        try
+        {
+            MapSpawnManager.Instance.SpawnedMap.monsterManager.AddList(this);
+        }
+        catch
+        {
+            Debug.Log("there is no MapspawnManager");
+        }
+    }
 
     // character controller
     public void Flip(bool isFlip)
     {
         transform.rotation = Quaternion.Euler(0, isFlip ? 0 : 180, 0);
     }
-    
+
     public void GetDamage(float damage)
     {
         statusHandler.isHit = true;
         btMachine.Notify();
         health -= damage;
-        
-        if(health <= 0) Die();
+
+        if (health <= 0) Die();
     }
 
     private void FixedUpdate()
@@ -62,7 +72,14 @@ public class EnemyController : MonoBehaviour, IDamagable
     // 리워드 표시, 리스폰 아리어에서 제거
     public void Die()
     {
-        Destroy(gameObject);
+        try
+        {
+            MapSpawnManager.Instance.SpawnedMap.monsterManager.RemoveEnemy(this);
+        }
+        catch
+        {
+            Destroy(gameObject);
+        }
 
         if (respawnArea != null)
         {
--- Assets/01.Scripts/Battle/Enemies/Controllers/FlyingController.cs
+++ Assets/01.Scripts/Battle/Enemies/Controllers/FlyingController.cs
@@ -1,7 +1,8 @@
 public class FlyingController : EnemyController
 {
-    protected override void Init()
+    public override void Start()
     {
+        base.Start();
         btMachine.Define(
                 new SelectorNode(new SequenceNode(new HitNode()), 
                 new SequenceNode(new FlyingIdleNode(duration: 1f), new FlyingPatrolAttackNode(duration: 1f)))
