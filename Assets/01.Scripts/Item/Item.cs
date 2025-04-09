824cecec31a5eea4c86a923639b52b870feaf71b
sinjid7 <wldmstls01@naver.com>
2025-04-09 19:16:10 +0900

    Fix: 인벤토리 수정

--- Assets/01.Scripts/Item/Item.cs
+++ Assets/01.Scripts/Item/Item.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ItemType
+{
+    Equipment,
+    Consumable
+}
+
 [System.Serializable]
 public class Item
 {
+    public ItemType itemType;
     public string itemName;
     public Sprite itemIcon;
+
+    public bool Use()
+    {
+        return false; //아이템 사용 성공 여부 반환
+    }
 
     public Item(string name)
     {
--- Assets/01.Scripts/UI/InventoryUI.cs
+++ Assets/01.Scripts/UI/InventoryUI.cs
@@ -1,19 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
+    Inventory inven;
+
     [SerializeField] private GameObject inventoryPanel; // 인벤토리 ui
     public Button closeButton; // 인벤 닫기 버튼
+
     bool activeInventory = false;
+
+    public Slot[] slots;
+    public Transform slotHolder;
+
+    private void Awake()
+    {
+        inven = Inventory.invenInstance; // Inventory 싱글톤 초기화
+        // slotHolder가 유효한지 체크 후 초기화
+        if (slotHolder != null)
+        {
+            slots = slotHolder.GetComponentsInChildren<Slot>(); // 슬롯 배열 초기화
+        }
+        else
+        {
+            Debug.LogError("slotHolder is not assigned!"); // slotHolder가 할당되지 않으면
+        }
+    }
 
     private void Start()
     {
-        inventoryPanel.SetActive(activeInventory);
-        closeButton.onClick.AddListener(CloseInventory);
+        inven.onSlotCountChange += SlotChange; // 슬롯 개수 변화시 호출될 메서드
+        inventoryPanel.SetActive(activeInventory); // 인벤창 상태 초기화
+        closeButton.onClick.AddListener(CloseInventory); // 인벤창 닫기버튼 이벤트
+    }
+
+    private void SlotChange(int val)
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inven.SlotCount)
+                slots[i].GetComponent<Button>().interactable = true; // 슬롯 활성화 버튼
+            else
+                slots[i].GetComponent<Button>().interactable = false; // 슬롯 활성화 버튼
+        }
     }
 
     private void Update()
@@ -34,5 +68,10 @@ public class InventoryUI : MonoBehaviour
     {
         activeInventory = false;
         inventoryPanel.SetActive(activeInventory);
+    }
+
+    public void AddSlot()
+    {
+        inven.SlotCount++; // 슬롯 추가
     }
 }
