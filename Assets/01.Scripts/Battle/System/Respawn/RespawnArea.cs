a89c2411b14ae05e7e72131edbac50ebfd3fef29
Junebeom Lee <junebeom.dev@gmail.com>
2025-04-09 10:48:55 +0900

    chore: 몬스터와 플레이어 상호작용 테스트

--- Assets/01.Scripts/Battle/System/Respawn/RespawnArea.cs
+++ Assets/01.Scripts/Battle/System/Respawn/RespawnArea.cs
@@ -6,9 +6,27 @@ public class RespawnArea : MonoBehaviour
     public Rect respawnArea;
     
     public List<GameObject> enemyList;
+    public List<GameObject> currEnemies;
+    public float cooldown;
     
     public int maxCount;
     private int currCount = 0;
+
+    public void SpawnEnemies()
+    {
+    }
+
+    public void DestroyEnemy(GameObject selectedEnemy)
+    {
+        var existEnemy = currEnemies.Find(enemy => enemy == selectedEnemy);
+        if (!existEnemy)
+        {
+            Debug.Log("respawnArea: enemy is not in area");
+            return;
+        }
+
+        currEnemies.Remove(existEnemy);
+    }
     
     private void OnDrawGizmos()
     {
