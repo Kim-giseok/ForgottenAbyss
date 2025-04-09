26f6ec7ef635ac0869f6525ea01fc77bc6fc2227
sinjid7 <wldmstls01@naver.com>
2025-04-09 15:30:17 +0900

    Add: 인벤창 버튼 추가

--- Assets/01.Scripts/UI/InventoryUI.cs
+++ Assets/01.Scripts/UI/InventoryUI.cs
@@ -2,23 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    public GameObject inventoryPanel; // 인벤토리 ui
+    [SerializeField] private GameObject inventoryPanel; // 인벤토리 ui
+    public Button closeButton; // 인벤 닫기 버튼
     bool activeInventory = false;
 
     private void Start()
     {
         inventoryPanel.SetActive(activeInventory);
+        closeButton.onClick.AddListener(CloseInventory);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
-        {
-            activeInventory = !activeInventory;
-            inventoryPanel.SetActive(activeInventory);
-        }
+            ActiveInventory();
+    }
+
+    // 인벤 활성화/ 비활성화 함수
+    void ActiveInventory()
+    {
+        activeInventory = !activeInventory;
+        inventoryPanel.SetActive(activeInventory);
+    }
+
+    // 인벤 닫기 ㅎ함수
+    void CloseInventory()
+    {
+        activeInventory = false;
+        inventoryPanel.SetActive(activeInventory);
     }
 }
