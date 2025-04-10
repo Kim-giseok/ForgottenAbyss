c585e10ca8aa1179d30b65549f384d3f70182c8b
Kim-giseok <belbeck@naver.com>
2025-04-10 16:37:43 +0900

    Feat: 맵 추가 제작
    맵 하나를 추가 제작해서 스테이지를 클리어 했을 때 다음 맵으로 넘어가게 했습니다.

--- Assets/01.Scripts/Manager/MapSpawnManager.cs
+++ Assets/01.Scripts/Manager/MapSpawnManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapSpawnManager : Singleton<MapSpawnManager>
 {
     [SerializeField] private Map[] maps;
+    int mapIdx = 0;
     public Map SpawnedMap { get; private set; }
 
     private void Awake()
     {
         _instance = this;
+        mapIdx = 0;
     }
 
     private void Start()
@@ -22,7 +25,13 @@ public class MapSpawnManager : Singleton<MapSpawnManager>
         if (SpawnedMap != null)
             Destroy(SpawnedMap.gameObject);
 
-        SpawnedMap = Instantiate(maps[Random.Range(0, maps.Length)]);
+        if (mapIdx >= maps.Length)
+        {
+            SceneManager.LoadScene(0); //이후 마을 신으로 수정
+            return;
+        }
+
+        SpawnedMap = Instantiate(maps[mapIdx++]);
         SpawnedMap.MapStart();
     }
 }
