5a905cd992a9518e1e8d955c9aaa0b983d18ce2c
sinjid7 <wldmstls01@naver.com>
2025-04-11 21:37:34 +0900

    Fix: 드래그 중 아이템이 UI위에 보이도록 수정

--- Assets/01.Scripts/UI/ItemUI.cs
+++ Assets/01.Scripts/UI/ItemUI.cs
@@ -12,11 +12,16 @@ public class ItemUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     private RectTransform rectTransform; // 드래그 UI 위치 설정
     private Image iconImage;
 
+    private Transform originalParent; // 원래 부모 저장
+    private Canvas dragCanvas; // 가장 위에 있는 Canvas
+
     private void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>();
         rectTransform = GetComponent<RectTransform>();
         iconImage = GetComponent<Image>(); // 아이콘 변경용
+
+        dragCanvas = GameObject.Find("DragCanvas").GetComponent<Canvas>(); // 드래그용
     }
 
     // 아이템 설정
@@ -38,6 +43,8 @@ public class ItemUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     // 드래그 시작
     public void OnBeginDrag(PointerEventData eventData)
     {
+        originalParent = transform.parent;
+        transform.SetParent(dragCanvas.transform, true); // 최상위 캔버스로 이동
         canvasGroup.alpha = 0.6f;   // 드래그 시 투명도
         canvasGroup.blocksRaycasts = false;  // 다른 UI와 충돌하지 않게 설정
     }
@@ -51,6 +58,7 @@ public class ItemUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     // 드래그 끝
     public void OnEndDrag(PointerEventData eventData)
     {
+        transform.SetParent(originalParent, true); // 다시 원래 부모로
         canvasGroup.alpha = 1f;   // 드래그 끝나고 원래 상태로
         canvasGroup.blocksRaycasts = true;
     }
