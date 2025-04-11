c257d352974140bd6be42d01cbdb9cd576839779
Kim-giseok <belbeck@naver.com>
2025-04-11 11:21:17 +0900

    Feat: 카메라 시네머신 적용
    카메라에 시네머신을 부착하고 맵 외각을 촬영하지 않도록 합니다.

--- Assets/01.Scripts/Manager/GameManager.cs
+++ Assets/01.Scripts/Manager/GameManager.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
     public Player player;
+    Camera mainCamera;
 
     private void Awake()
     {
         _instance = this;
         player = FindObjectOfType<Player>();
+
+        mainCamera = Camera.main;
+        if (mainCamera.TryGetComponent<CinemachineBrain>(out var component))
+            component = mainCamera.AddComponent<CinemachineBrain>();
     }
 }
