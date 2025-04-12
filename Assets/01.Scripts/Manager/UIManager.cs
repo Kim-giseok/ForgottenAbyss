6fc23486b3592f9fcb14908c545039e7f77832ad
sinjid7 <wldmstls01@naver.com>
2025-04-12 17:58:02 +0900

    Feat: 설정창 esc로 열기/닫기

--- Assets/01.Scripts/Manager/UIManager.cs
+++ Assets/01.Scripts/Manager/UIManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    public InventoryUI inventoryUI;  // 인벤토리 UI
+    public InventoryUI inventoryUI;  // 인벤토리
+    public SettingsMenu settingsMenu; // 옵션창
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))  // 인벤토리 열기/닫기
         {
             inventoryUI.ToggleInventory();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) // 옵션창 열기/닫기
+        {
+            settingsMenu.ToggleSettingsMenu();
         }
     }
 }
