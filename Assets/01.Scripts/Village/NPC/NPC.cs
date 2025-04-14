3f07d22cb6a113d5ad81c62d2e4542e658235b1a
Lee Jae Hyu <wogb0918@gmail.com>
2025-04-14 18:20:06 +0900

    Chore: 코드 수정

--- Assets/01.Scripts/Village/NPC/NPC.cs
+++ Assets/01.Scripts/Village/NPC/NPC.cs
@@ -9,52 +9,40 @@ public class NPC : MonoBehaviour, IInteractable
 {
     NpcSentence npcSentence;
     PlayerInput playerInput;
-    public float interactionRange; //상호작용 범위
-    public LayerMask interactableLayer; //상호작용 가능한 오브젝트의 레이어
     public GameObject npcText; //npc에게 다가가면 활성화 되는 안내문구
 
     private void Awake()
     {
         npcSentence = GetComponent<NpcSentence>();
         playerInput = GameObject.FindWithTag("Player").GetComponent<PlayerInput>();
-    }
-
-    private void Update()
-    {
-        Vector2 rayOrigin = new Vector2(transform.position.x, transform.position.y ); ; // Raycast 시작점
-        Vector2 rayDirection = transform.right; // 오른쪽 방향으로 Raycast
-
-        Debug.DrawRay(rayOrigin, rayDirection * interactionRange, Color.red);
-
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, 1f, interactableLayer);
-
-        if (hit.collider != null && hit.collider.CompareTag("Player"))
-        {
-            if (FindObjectOfType<TalkSystem>() != null)
-            {
-                npcText.SetActive(false); //대화창이 켜져 있으면 안내문구 비활성화
-                return;
-            }
-            else
-            {
-                npcText.SetActive(true);
-            }
-
-        }
-        else
-        {
-            npcText.SetActive(false);
-        }
+        npcText.SetActive(false);
     }
 
     public void ActiveInteraction()
     {
         playerInput.enabled = false;
+        npcText.SetActive(false);
         npcSentence.TalkNpc();
     }
 
     public void ReadyInteraction()
     {
 
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null && collision.CompareTag("Player"))
+        {
+            npcText.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision != null && collision.CompareTag("Player"))
+        {
+            npcText.SetActive(false);
+        }
     }
 }
