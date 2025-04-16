35da2c8d8d44fb9302fc0f51b98a901847504696
Kim-giseok <belbeck@naver.com>
2025-04-16 11:44:15 +0900

    Fix: 누르는 레버 레이어 조건 추가
    플레이어 레이어가 누를 때만 반응하도록 합니다.

--- Assets/01.Scripts/MapObject/Laber/LaberPush.cs
+++ Assets/01.Scripts/MapObject/Laber/LaberPush.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class LaberPush : LaberBase
 {
     [SerializeField] Collider2D pushCollider;
+    [SerializeField] LayerMask playerLayer;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if ((playerLayer & (1 << collision.gameObject.layer)) <= 0) return;
+
         Vector2 direct = collision.transform.position - transform.position;
         float projectionLength = Vector2.Dot(direct, transform.up);
 
