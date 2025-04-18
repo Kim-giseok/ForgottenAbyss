63c70fe59ce7ebcb4cd1113f92b025c720947de0
Junebeom Lee <junebeom.dev@gmail.com>
2025-04-18 11:12:47 +0900

    Chore: 힐러 테스트

--- Assets/01.Scripts/Battle/BehaviourTree/Nodes/MoveNodes.cs
+++ Assets/01.Scripts/Battle/BehaviourTree/Nodes/MoveNodes.cs
@@ -87,6 +87,8 @@ public class TracingNode : Node
     
     public override void Update()
     {
+        // 플랫폼을 우선 체크하여 넘어갈 수 없는 상황이라면 점프(이때 
+        
         controller.LookTarget();
         // bug: 추적 방향이 위쪽이면 속도까지 줄어드는 문제 발생
         controller.rigidbody.velocity = new Vector2(controller.agent.GetDirection().x * controller.agent.tracingSpeed, controller.rigidbody.velocity.y);
