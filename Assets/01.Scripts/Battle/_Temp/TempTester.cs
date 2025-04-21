5f92da946d3cecee5230b34858af0caba8b4cf3c
Junebeom Lee <junebeom.dev@gmail.com>
2025-04-21 16:39:25 +0900

    Chore: 테스트

--- Assets/01.Scripts/Battle/_Temp/TempTester.cs
+++ Assets/01.Scripts/Battle/_Temp/TempTester.cs
@@ -11,34 +11,19 @@ public class TempTester: MonoBehaviour
    {
       collider = GetComponent<BoxCollider2D>();
    }
-
-
+   
    private void Update()
    {
-      RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, LayerMask.GetMask("Ground"));
-      if (!hit.collider) return;
+      if (Input.GetKeyDown(KeyCode.K))
+      {
+         // direction 정보를 넘길 수 없음
+         ProjectileManager.Instance.CreateSummon(transform, SummonSkillManager.Skill.BossSkill3, false);
+      }
       
-      Vector3 hitPoint = hit.point;
-      
-      // hit point의 모서리 충돌로 인한 한칸 보정 필요
-      var cellPos = NavSurface.tilemap.WorldToCell(hitPoint);
-      var curTile = NavSurface.cells.Find(cell => cell.tilePos == new Vector3Int(cellPos.x, cellPos.y - 1, 0));
-      
-      if (curTile != null) { Debug.Log(curTile.platformID); }
+      if (Input.GetKeyDown(KeyCode.L))
+      {
+         ProjectileManager.Instance.CreateSummon(transform, SummonSkillManager.Skill.DashAttack, true);
+      }
    }
-   
-   // private void Update()
-   // {
-   //    if (Input.GetKeyDown(KeyCode.K))
-   //    {
-   //       // direction 정보를 넘길 수 없음
-   //       ProjectileManager.Instance.CreateSummon(transform, SummonSkillManager.Skill.BossSkill3, false);
-   //    }
-   //    
-   //    if (Input.GetKeyDown(KeyCode.L))
-   //    {
-   //       ProjectileManager.Instance.CreateSummon(transform, SummonSkillManager.Skill.DashAttack, true);
-   //    }
-   // }
 
 }
