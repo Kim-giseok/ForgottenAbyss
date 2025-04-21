c59e3b141e0a04f21a47aa3647a4544963e2c34e
Kim-giseok <belbeck@naver.com>
2025-04-21 19:32:10 +0900

    Fix: 배경 움직임 lateupdate로 수정

--- Assets/01.Scripts/Manager/MapSpawnManager.cs
+++ Assets/01.Scripts/Manager/MapSpawnManager.cs
@@ -29,7 +29,7 @@ public class MapSpawnManager : Singleton<MapSpawnManager>
         SpawnNextMap();
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         ParrallexScrollBackground();
     }
