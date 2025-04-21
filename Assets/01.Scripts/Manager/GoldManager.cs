4227f5cb6eff24851ca08eff7e84ce8e6e2569b7
sinjid7 <wldmstls01@naver.com>
2025-04-21 21:13:28 +0900

    Feat: 골드매니저 스크립트

--- Assets/01.Scripts/Manager/GoldManager.cs
+++ Assets/01.Scripts/Manager/GoldManager.cs
@@ -1,27 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GoldManager : MonoBehaviour
 {
     public static GoldManager Instance { get; private set; }
-    [SerializeField] private int currentGold = 1000;
+
+    [SerializeField] private int currentGold = 1000; // 현재 소지 금액
+    [SerializeField] private List<TextMeshProUGUI> goldTexts;
 
     private void Awake()
     {
         // 싱글톤 패턴
         if (Instance == null)
-        {
             Instance = this;
-        }
         else
-        {
             Destroy(gameObject);
-        }
+
+        UpdateGoldUI();
     }
 
-    public int GetGold()
+    public int GetGold() => currentGold;
+
+    public bool SpendGold(int amount)
     {
-        return currentGold;
+        if (currentGold >= amount)
+        {
+            currentGold -= amount;
+            UpdateGoldUI();
+            return true;
+        }
+        return false;
+    }
+
+    public void AddGold(int amount)
+    {
+        currentGold += amount;
+        UpdateGoldUI();
+    }
+
+    private void UpdateGoldUI()
+    {
+        foreach (var goldtxt in goldTexts)
+        {
+            goldtxt.text = currentGold.ToString() + "";
+        }
     }
 }
