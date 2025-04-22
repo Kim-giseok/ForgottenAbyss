b72693f0d2db7f9a4c31868e6c627cd9d86efbe3
Kim-giseok <belbeck@naver.com>
2025-04-22 12:41:09 +0900

    Fix: 게이트 진입 수정
    상호작용을 통해 게이트에 진입하도록 합니다.

--- Assets/01.Scripts/Village/EnterGate.cs
+++ Assets/01.Scripts/Village/EnterGate.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EnterGate : MonoBehaviour
+public class EnterGate : MonoBehaviour, IInteractable
 {
     public ConfirmationUI confirmationUI;
     public int gateNumber; //문 고유 번호
@@ -13,29 +13,20 @@ public class EnterGate : MonoBehaviour
     {
         confirmationUI.gameObject.SetActive(false);
     }
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision != null && collision.CompareTag("Player"))
-        {
-            confirmationUI.PopUpUI(OnClickEnter);
-            Debug.Log(gateNumber);
-        }
-    }
-
-    public void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision == null || confirmationUI == null || !gameObject.activeInHierarchy)
-            return;
-        if (collision != null && collision.CompareTag("Player"))
-        {
-            if (confirmationUI != null)
-                confirmationUI.gameObject.SetActive(false);
-        }
-    }
 
     public void OnClickEnter()
     {
         Debug.Log(gateNumber.ToString());
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void ReadyInteraction()
+    {
+    }
+
+    public void ActiveInteraction()
+    {
+            confirmationUI.PopUpUI(OnClickEnter);
+            Debug.Log(gateNumber);
     }
 }
