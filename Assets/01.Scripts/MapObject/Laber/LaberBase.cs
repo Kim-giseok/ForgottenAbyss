183b359d79e01a88e56a41da8fc603c2c7eb156b
Kim-giseok <belbeck@naver.com>
2025-04-29 14:31:57 +0900

    Fix: 1회 작동을 레버로 변경

--- Assets/01.Scripts/MapObject/Laber/LaberBase.cs
+++ Assets/01.Scripts/MapObject/Laber/LaberBase.cs
@@ -2,19 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FLAGKEY
+{
+    DEFAULTFLAG,
+    INTRO_DIALOGUE
+}
+
+public static class ActivateFlag
+{
+    static Dictionary<FLAGKEY, bool> flags = new();
+
+    public static void ActiveFlag(FLAGKEY key)
+    {
+        flags[key] = true;
+    }
+
+    public static bool CheckFlag(FLAGKEY key)
+    {
+        return flags.ContainsKey(key) && flags[key];
+    }
+}
+
 public class LaberBase : MonoBehaviour
 {
     [SerializeField] Machine[] targetMachines;
     [SerializeField] Animator laberAnim;
     protected bool isSwitched = false;
 
+    [Header("Once active parameter")]
+    [SerializeField] bool activeOnce;
+    [SerializeField] FLAGKEY flagName = FLAGKEY.DEFAULTFLAG;
+
+    bool isAleadyActivated => activeOnce && ActivateFlag.CheckFlag(flagName);
+
     public virtual void SwitchMachine()
     {
-        if (isSwitched) return;
+        if (isSwitched || isAleadyActivated) return;
+
         laberAnim?.SetFloat("Active", 1);
         foreach (var targetMachine in targetMachines)
             targetMachine.Active(this);
+
         isSwitched = true;
+        ActivateFlag.ActiveFlag(flagName);
     }
 
     public virtual void DisSwitchMachine()
--- Assets/01.Scripts/MapObject/Machine/Machine.cs
+++ Assets/01.Scripts/MapObject/Machine/Machine.cs
@@ -3,45 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum FLAGKEY
-{
-    DEFAULTFLAG,
-    INTRO_DIALOGUE
-}
-
-public static class ActivateFlag
-{
-    static Dictionary<FLAGKEY, bool> flags = new();
-
-    public static void ActiveFlag(FLAGKEY key)
-    {
-        flags[key] = true;
-    }
-
-    public static bool CheckFlag(FLAGKEY key)
-    {
-        return flags.ContainsKey(key) && flags[key];
-    }
-}
-
 public class Machine : MonoBehaviour
 {
     [SerializeField] protected Animator machineAnim;
     [SerializeField] protected bool isActivated = false;
 
-    [Header("Once active parameter")]
-    [SerializeField] bool activeOnce;
-    [SerializeField] FLAGKEY flagName = FLAGKEY.DEFAULTFLAG;
-
-    bool isAleadyActivated => activeOnce && ActivateFlag.CheckFlag(flagName);
-
     public virtual void Active(LaberBase rootlaber)
     {
-        if (isActivated || isAleadyActivated) return;
+        if (isActivated) return;
         machineAnim.SetFloat("Active", 1);
         isActivated = true;
 
-        ActivateFlag.ActiveFlag(flagName);
         Debug.Log(name + " activated");
     }
 
