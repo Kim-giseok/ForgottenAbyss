d47e8f851cdb9a8a2a6fe42e1733a11ec7aba4ed
Junebeom Lee <junebeom.dev@gmail.com>
2025-04-29 10:58:38 +0900

    Fix: swordShadow 히트박스 수정

--- Assets/01.Scripts/Battle/Enemies/Enemies/SwordShadow.cs
+++ Assets/01.Scripts/Battle/Enemies/Enemies/SwordShadow.cs
@@ -10,7 +10,6 @@ public class SSDashAttack : Node
 
     public override void Start()
     {
-        
         controller.animnHandler.Play(animationName);
         controller.LookTarget();
 
@@ -19,20 +18,14 @@ public class SSDashAttack : Node
         controller.rigidbody.drag = 4f;
         controller.rigidbody.AddForce(new Vector2(controller.agent.GetDirection().x * 24f, 0), ForceMode2D.Impulse);
     }
-
-    // public override void OnAnimatedEvent(bool isFire)
-    // {
-        // if(isFire) ProjectileManager.Instance.CreateMeleeProjectile(controller.transform, 10f);
-        // else ProjectileManager.Instance.DestroyMeleeProjectile(controller.transform);
-    // }
-
+    
     public override void OnAnimated(AnimationStatus status, AnimatorStateInfo animInfo)
     {
         // notice: 플래그로 관리 필요
         if (!animInfo.IsName("Combo1") && !animInfo.IsName("Combo2") && !animInfo.IsName("Combo3")) return;
         if (status == AnimationStatus.Start)
         {
-            BoltsPool.Instance.CreateMelee(controller.transform, 10f);
+            BoltsPool.Instance.CreateMelee(controller.transform).SetDamage(10).Fire();
         }
         
         if (status == AnimationStatus.End)
