8fe7f6544754df2cdb4c043d0103dd68b357eec6
Lee Jae Hyu <wogb0918@gmail.com>
2025-04-30 19:28:29 +0900

    Refactor: 플레이어 사운드 스크립트 수정

--- Assets/01.Scripts/Character/Player/PlayerSound.cs
+++ Assets/01.Scripts/Character/Player/PlayerSound.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerSound : MonoBehaviour
 {
     // 오디오 소스 참조
-    private AudioSource audioSource;
+    //private AudioSource audioSource;
+
+    
 
     // 다양한 동작 사운드들
     public AudioClip jumpSound;
@@ -15,33 +17,33 @@ public class PlayerSound : MonoBehaviour
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
 
     }
 
     public void JumpSound()
     {
-        if (audioSource != null && jumpSound != null)
+        if (jumpSound != null)
         {
-            audioSource.PlayOneShot(jumpSound);
+            SoundManager.Instance.PlaySFX(jumpSound);
             
         }
     }
 
     public void LandSound()
     {
-        if (audioSource != null && landSound != null)
+        if (landSound != null)
         {
-            audioSource.PlayOneShot(landSound);
+            SoundManager.Instance.PlaySFX(landSound);
             
         }
     }
 
     public void DashSound()
     {
-        if (audioSource != null && dashSound != null)
+        if (dashSound != null)
         {
-            audioSource.PlayOneShot(dashSound);
+            SoundManager.Instance.PlaySFX(dashSound);
 
         }
     }
