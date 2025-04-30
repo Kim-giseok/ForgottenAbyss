545a6fca15fcce3106b8975aa7090870dedc4536
daeng98 <daeng9821@gmail.com>
2025-04-30 19:29:47 +0900

    Chore: 장착 세이브 일단 주석처리
    장착 세이브 일단 주석처리

--- Assets/01.Scripts/Equipment/Armor/Manager/EquipmentManager.cs
+++ Assets/01.Scripts/Equipment/Armor/Manager/EquipmentManager.cs
@@ -246,7 +246,7 @@ public class EquipmentManager : MonoBehaviour
     private void OnApplicationQuit()
     {
         Debug.Log("[Auto] 애플리케이션 종료 → 장비 저장");
-        SaveEquippedArmors();
+        //SaveEquippedArmors();
     }
 
     public void EquipMemoryPiece(MemoryPieceSO memorySO)
