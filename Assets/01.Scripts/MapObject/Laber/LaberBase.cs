2a0b2813447654c31565256b435dda1611e76a5a
Kim-giseok <belbeck@naver.com>
2025-05-07 14:53:45 +0900

    Feat: 무기획득 플래그 추가, 플래그 저장 기능 작성
    플래그 저장은 아직 정상적으로 동작하지 않습니다.

--- Assets/01.Scripts/MapObject/Laber/LaberBase.cs
+++ Assets/01.Scripts/MapObject/Laber/LaberBase.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public enum FLAGKEY
@@ -10,12 +12,30 @@ public enum FLAGKEY
     WEAPON_SWAP_TUTORIAL,
     STAGE_1_CLEAR,
     STAGE_2_CLEAR,
-    STAGE_3_CLEAR
+    STAGE_3_CLEAR,
+    FIRST_WEAPON_EQUIP
 }
 
 public static class ActivateFlag
 {
     static Dictionary<FLAGKEY, bool> flags = new();
+    static string flagSabePath = "activateFlags.json";
+
+    static ActivateFlag()
+    {
+        var flagSaveData = flags.ToList();
+        flagSaveData = DataSave<List<KeyValuePair<FLAGKEY, bool>>>.LoadOrBase(flagSaveData, flagSabePath);
+
+#if UNITY_EDITOR
+        EditorApplication.playModeStateChanged -= SaveFlags;
+        EditorApplication.playModeStateChanged += SaveFlags;
+#elif UNITY_WEBGL
+
+#else
+        Application.quitting -= SaveFlags;
+        Application.quitting += SaveFlags;
+#endif
+    }
 
     public static void ActiveFlag(FLAGKEY key)
     {
@@ -25,6 +45,18 @@ public static class ActivateFlag
     public static bool CheckFlag(FLAGKEY key)
     {
         return flags.ContainsKey(key) && flags[key];
+    }
+
+    static void SaveFlags()
+    {
+        var flagSaveData = flags.ToList();
+        DataSave<List<KeyValuePair<FLAGKEY,bool>>>.SaveData(flagSaveData, flagSabePath);
+    }
+
+    static void SaveFlags(PlayModeStateChange mode)
+    {
+        if (mode == PlayModeStateChange.ExitingPlayMode)
+            SaveFlags();
     }
 }
 
