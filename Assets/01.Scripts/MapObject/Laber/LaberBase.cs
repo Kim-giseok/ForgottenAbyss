4cdb76fb6df6a929dc101657b0efcf1346b8a50b
Kim-giseok <belbeck@naver.com>
2025-05-07 15:33:18 +0900

    Refactor: ActiveFlag새 파일로 분리

--- Assets/01.Scripts/MapObject/Laber/LaberBase.cs
+++ Assets/01.Scripts/MapObject/Laber/LaberBase.cs
@@ -4,62 +4,6 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-public enum FLAGKEY
-{
-    DEFAULTFLAG,
-    INTRO_DIALOGUE,
-    BASE_BATTLE_TUTORIAL,
-    WEAPON_SWAP_TUTORIAL,
-    STAGE_1_CLEAR,
-    STAGE_2_CLEAR,
-    STAGE_3_CLEAR,
-    FIRST_WEAPON_EQUIP
-}
-
-public static class ActivateFlag
-{
-    static Dictionary<FLAGKEY, bool> flags = new();
-    static string flagSabePath = "activateFlags.json";
-
-    static ActivateFlag()
-    {
-        var flagSaveData = flags.ToList();
-        flagSaveData = DataSave<List<KeyValuePair<FLAGKEY, bool>>>.LoadOrBase(flagSaveData, flagSabePath);
-
-#if UNITY_EDITOR
-        EditorApplication.playModeStateChanged -= SaveFlags;
-        EditorApplication.playModeStateChanged += SaveFlags;
-#elif UNITY_WEBGL
-
-#else
-        Application.quitting -= SaveFlags;
-        Application.quitting += SaveFlags;
-#endif
-    }
-
-    public static void ActiveFlag(FLAGKEY key)
-    {
-        flags[key] = true;
-    }
-
-    public static bool CheckFlag(FLAGKEY key)
-    {
-        return flags.ContainsKey(key) && flags[key];
-    }
-
-    static void SaveFlags()
-    {
-        var flagSaveData = flags.ToList();
-        DataSave<List<KeyValuePair<FLAGKEY,bool>>>.SaveData(flagSaveData, flagSabePath);
-    }
-
-    static void SaveFlags(PlayModeStateChange mode)
-    {
-        if (mode == PlayModeStateChange.ExitingPlayMode)
-            SaveFlags();
-    }
-}
-
 public class LaberBase : MonoBehaviour
 {
     [SerializeField] Machine[] targetMachines;
