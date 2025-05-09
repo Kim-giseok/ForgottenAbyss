220f5e8f017bec33ef79b8a48c6265e0c98a38e1
Junebeom Lee <junebeom.dev@gmail.com>
2025-05-09 12:42:15 +0900

    Chore: Tip에서 중복 문구 안나오도록 적용

--- Assets/01.Scripts/UI/TipUI.cs
+++ Assets/01.Scripts/UI/TipUI.cs
@@ -10,9 +10,15 @@ public class TipUI: MonoBehaviour
     [System.Serializable] public class Tip { public string title; public string desc; }
     public List<Tip> tips;
 
+    public int currIndex;
     private void Shuffle()
     {
-        Tip currTip = tips[Random.Range(0, tips.Count)];
+        int newIndex;
+        do { newIndex = Random.Range(0, tips.Count); }
+        while (newIndex == currIndex);
+        currIndex = newIndex;
+       
+        Tip currTip = tips[currIndex];
         titleText.text = currTip.title;
         descText.text = currTip.desc;
     }
