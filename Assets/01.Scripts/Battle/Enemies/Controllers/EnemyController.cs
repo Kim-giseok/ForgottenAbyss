61c99f311488bef03b934d5b3fccd590f6f3a9ae
Junebeom Lee <junebeom.dev@gmail.com>
2025-05-13 15:48:27 +0900

    Feat: 피격 VFX 추가

--- Assets/01.Scripts/Battle/Enemies/Controllers/EnemyController.cs
+++ Assets/01.Scripts/Battle/Enemies/Controllers/EnemyController.cs
@@ -119,7 +119,7 @@ public class EnemyController : EnemyBaseController, IDamagable
         resourceHandler.Modify(EnemyStatType.Health, -damage);
         statusHandler.SetMode(EnmeyMode.Hit, true);
         BoltsPool.Instance.CreateParticle(transform, Random.Range(0, 2) == 1 ? "Hit" : "Hit2")
-            .SetSize(0.1f).SetColor(Color.yellow).SetPosition(transform.position + new Vector3(Random.Range(-0.2f, 0.2f), 0.5f + Random.Range(-0.2f, 0.2f))).Play();
+            .SetSize(0.1f).SetColor(Color.yellow).SetPosition(transform.position + new Vector3(Random.Range(-0.2f, 0.2f), 0.6f + Random.Range(-0.2f, 0.2f))).Play();
         
         // 방어력 개념도 구현하기
         // statusHandler.stamina -= 1;
