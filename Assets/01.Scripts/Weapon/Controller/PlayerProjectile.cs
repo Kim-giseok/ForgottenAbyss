1bbe20966264b53b88627bf4f00fa6c8fa69a4dd
Junebeom Lee <junebeom.dev@gmail.com>
2025-05-14 11:40:18 +0900

    Chore: 카메라 흔들림 강조 조정

--- Assets/01.Scripts/Weapon/Controller/PlayerProjectile.cs
+++ Assets/01.Scripts/Weapon/Controller/PlayerProjectile.cs
@@ -57,7 +57,7 @@ public class PlayerProjectile : MonoBehaviour
 
                     Vector3 textPosition = other.transform.position + Vector3.up * 1f;
                     DamageTextManager.Instance.ShowDamage(textPosition, (int)result.damage, result.isCrit);
-                    GameManager.Instance.cameraShake.Shake(0.1f, 0.2f);
+                    GameManager.Instance.cameraShake.Shake(0.1f, 0.6f);
                 }
 
                 if(other.GetComponent<LaberDamagerble>() != null)
