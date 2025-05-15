835d73a8ca46ca509b5b08300d660eb8f5977292
Kim-giseok <belbeck@naver.com>
2025-05-15 12:52:28 +0900

    Fix: 줄타기 상태 수정
    줄을 탓을 때 맵 중앙으로 이동 삭제.
    줄 타는 중 플랫폼을 무시하도록 설정
    줄타는 중 점프가 가능하도록 설정

--- Assets/01.Scripts/Character/Player/State/ClimbState.cs
+++ Assets/01.Scripts/Character/Player/State/ClimbState.cs
@@ -7,104 +7,65 @@ public class ClimbState : PlayerStateMachine
 {
     public float climbSpeed = 5f;
     public float originalGravity;
-    public bool isMoving;
+
     public ClimbState(ControllerPlayer player) : base(player) { }
 
     public override void Enter()
     {
+        base.Enter();
         player.isOnLadder = true;
         player.animator.SetTrigger("LadderTrigger");
         player.animator.SetBool("IsLadder", true);
         Debug.Log("트리거 진입");
-        originalGravity = player.rigid.gravityScale;
 
-        Physics2D.IgnoreLayerCollision(player.gameObject.layer, LayerMask.NameToLayer("Ground"), true);
+        player.currentJumpCount = 0;
 
-        // 현재 접촉 중인 WallClimb 레이어의 트리거 찾기
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(player.transform.position, 1f);
-        Collider2D Collider = null;
-
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.gameObject.layer == LayerMask.NameToLayer("Climb"))
-            {
-                Collider = collider;
-                break;
-            }
-        }
-
-        // 트리거가 있다면 플레이어를 중앙으로 이동
-        if (Collider != null)
-        {
-            // 트리거의 중앙 X 위치 계산
-            Vector2 centerPosition = Collider.bounds.center;
-
-            // 플레이어의 X 위치만 트리거의 중앙으로 변경
-            Vector3 newPosition = player.transform.position;
-            newPosition.x = centerPosition.x;
-            player.transform.position = newPosition;
-        }
+        player.playerCollider.excludeLayers = player.platformLayerMask;
 
         // 수평 속도 0으로 설정
         player.rigid.velocity = new Vector2(0, 0);
+
+        originalGravity = player.rigid.gravityScale;
         player.rigid.gravityScale = 0;
     }
 
-    public override void Update()
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(player.transform.position, 1f);
-        Collider2D Collider = null;
-
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.gameObject.layer == LayerMask.NameToLayer("Climb"))
-            {
-                Collider = collider;
-                break;
-            }
-        }
-
-        if (player.inputVec.y > 0 && player.transform.position.y >= Collider.bounds.max.y - 1.1f)
-        {
-                       
-            Vector3 topPosition = new Vector3(Collider.bounds.center.x,
-                Collider.bounds.max.y + player.playerCollider.bounds.extents.y - 0.4f, 0 );
-
-            player.transform.position = topPosition;
-            player.ChangeState(PlayerState.Idle);
-            return;
-        }
-    }
     public override void FixedUpdate()
     {
+        base.FixedUpdate();
         // Y축 입력에 따라 상하 이동
         player.rigid.velocity = new Vector2(0, player.inputVec.y * climbSpeed);
     }
 
     public override void Exit()
     {
+        base.Exit();
         player.isOnLadder = false;
         player.animator.SetBool("IsLadder", false);
         player.rigid.gravityScale = originalGravity;
 
-        Physics2D.IgnoreLayerCollision(player.gameObject.layer, LayerMask.NameToLayer("Ground"), false);
+        player.playerCollider.excludeLayers = 0;
     }
 
     public override void OnJump()
     {
-        player.ChangeState(PlayerState.Idle);
+        base.OnJump();
+        if (player.CanJump)
+            player.ChangeState(PlayerState.Jump);
     }
+
     public override void OnCollisionExit(Collision2D collision)
     {
+        base.OnCollisionExit(collision);
         if (collision.gameObject.layer == LayerMask.NameToLayer("Climb"))
         {
             // 원래 상태로 돌아가기
             player.ChangeState(PlayerState.Idle);
         }
-
     }
+
     public override void OnTriggerExit(Collider2D collision)
     {
+        base.OnTriggerExit(collision);
         if (collision.gameObject.layer == LayerMask.NameToLayer("Climb"))
         {
             // 원래 상태로 돌아가기
