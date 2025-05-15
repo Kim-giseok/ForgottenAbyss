04a5f91863638930435b04dd501eb67cb4969540
sinjid7 <wldmstls01@naver.com>
2025-05-15 19:26:30 +0900

    WIP: 툴팁 리팩 중

---
+++ Assets/01.Scripts/Manager/TooltipManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipManager : MonoBehaviour
+{
+    [SerializeField] private ItemTooltip itemTooltip;
+}
