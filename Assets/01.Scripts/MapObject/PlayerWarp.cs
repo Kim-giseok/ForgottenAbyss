d54e3496b8c211a0e2ef468c42389ab26a5ba49e
Kim-giseok <belbeck@naver.com>
2025-05-16 16:50:38 +0900

    Fix: playerwarp x/y축 무시 기능 추가

--- Assets/01.Scripts/MapObject/PlayerWarp.cs
+++ Assets/01.Scripts/MapObject/PlayerWarp.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerWarp : MonoBehaviour
 {
+    [Header("Ignore position")]
+    [SerializeField] bool x;
+    [SerializeField] bool y;
+
     public void WarpPlayerToThis()
     {
         Player player = GameManager.Instance.player;
 
-        player.transform.position = transform.position;
+        player.transform.position =
+            new Vector3
+            (
+                x ? player.transform.position.x : transform.position.x,
+                y ? player.transform.position.y : transform.position.y,
+                0
+            );
         player.controller.rigid.velocity = Vector2.zero;
     }
 }
