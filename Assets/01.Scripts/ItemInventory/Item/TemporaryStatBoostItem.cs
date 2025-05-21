b6b6ce336d9eaa18a86ea2600c99d712f0b3899e
sinjid7 <wldmstls01@naver.com>
2025-05-21 17:51:36 +0900

    Refactor: 버프아이템 코드 수정

--- Assets/01.Scripts/ItemInventory/Item/TemporaryStatBoostItem.cs
+++ Assets/01.Scripts/ItemInventory/Item/TemporaryStatBoostItem.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewTemporaryStatBoostItem", menuName = "SO/Item/TemporaryStatBoostItem")]
@@ -9,38 +8,9 @@ public class TemporaryStatBoostItem : ConsumableItem
     public float boostAmount = 10f;
     public float duration = 10f;
 
-    private static HashSet<StatType> activeBuffs = new HashSet<StatType>();
-
     public override bool Use()
     {
         PlayerStatus player = GameObject.FindObjectOfType<PlayerStatus>();
-        if (player == null) return false;
-
-        if (activeBuffs.Contains(targetStat)) 
-        {
-            Debug.LogWarning($"[StatBoost] {targetStat} 버프는 이미 적용 중입니다!");
-            return false; 
-        }
-
-        player.StartCoroutine(ApplyTemporaryBoost(player));
-        return true;
-    }
-
-    private System.Collections.IEnumerator ApplyTemporaryBoost(PlayerStatus player)
-    {
-        activeBuffs.Add(targetStat);
-
-        float originalValue = player.GetStat(targetStat);
-        player.SetStat(targetStat, originalValue + boostAmount);
-        Debug.Log($"[TemporaryStatBoostItem] {targetStat} 스탯이 {boostAmount}만큼 증가했습니다. {duration}초 동안 유지됩니다.");
-
-        yield return new WaitForSeconds(duration);
-
-        float currentValue = player.GetStat(targetStat);
-        player.SetStat(targetStat, currentValue - boostAmount);
-        activeBuffs.Remove(targetStat);
-
-        Debug.Log($"[TemporaryStatBoostItem] {targetStat} 스탯 증가 효과가 종료되었습니다.");
-
+        return player != null && player.TryApplyBuff(targetStat, boostAmount, duration);
     }
 }
