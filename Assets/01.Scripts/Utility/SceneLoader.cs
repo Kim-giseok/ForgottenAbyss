23652aaa237a6302dd9bde4a681d8bef8a8d58fb
daeng98 <daeng9821@gmail.com>
2025-05-22 12:00:44 +0900

    Feat: 마우스 커서 추가
    마우스 커서 추가 해봤음
    임시로 getbutton으로 연결해둠
    사용안하면 해당 부분 다 제거하면 될듯

--- Assets/01.Scripts/Utility/SceneLoader.cs
+++ Assets/01.Scripts/Utility/SceneLoader.cs
@@ -8,7 +8,33 @@ public class SceneLoader : SingletonLoadRemain<SceneLoader>
     [HideInInspector] public string nextSceneName;
     public string loadingSceneName;
 
+    [SerializeField] private Texture2D normalCursor;
+    [SerializeField] private Texture2D clickedCursor;
+    [SerializeField] private Vector2 hotSpot = Vector2.zero;
+
     public FadeScene fade;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (normalCursor != null)
+        {
+            Cursor.SetCursor(normalCursor, hotSpot, CursorMode.Auto);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 클릭 시 커서 변경
+        {
+            Cursor.SetCursor(clickedCursor, hotSpot, CursorMode.Auto);
+        }
+        else if (Input.GetMouseButtonUp(0)) // 클릭 해제 시 원래 커서로 복구
+        {
+            Cursor.SetCursor(normalCursor, hotSpot, CursorMode.Auto);
+        }
+    }
 
     protected override void Init()
     {
