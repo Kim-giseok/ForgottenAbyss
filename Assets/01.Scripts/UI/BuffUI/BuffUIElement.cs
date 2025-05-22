d60a97827feb255e22c237e7ea94b4a61d827c36
sinjid7 <wldmstls01@naver.com>
2025-05-22 21:20:18 +0900

    Feat: 버프 쿨타임 이미지 추가
    + 버프아이템 두번째 사용시 쿨타임슬롯이 안나오는 버그 수정

--- Assets/01.Scripts/UI/BuffUI/BuffUIElement.cs
+++ Assets/01.Scripts/UI/BuffUI/BuffUIElement.cs
@@ -5,10 +5,12 @@ using TMPro;
 
 public class BuffUIElement : MonoBehaviour
 {
-    [SerializeField] private UnityEngine.UI.Image iconImage;
+    [SerializeField] private Image iconImage;
     [SerializeField] private TMP_Text timeText;
+    [SerializeField] private Image cooldownMask;
     [SerializeField] private CanvasGroup canvasGroup;
 
+    private float duration;
     private float timeLeft;
     private System.Action onEnd;
     private StatType stat;
@@ -17,10 +19,13 @@ public class BuffUIElement : MonoBehaviour
     {
         this.stat = stat;
         this.timeLeft = duration;
-        this.onEnd = onEndCallback;
+        this.duration = duration;
+        this.onEnd = onEndCallback;      
 
         iconImage.sprite = icon;
         timeText.text = Mathf.CeilToInt(timeLeft).ToString();
+        cooldownMask.fillAmount = 1f;
+        canvasGroup.alpha = 1f;
 
         StartCoroutine(UpdateTimer());
     }
@@ -31,12 +36,13 @@ public class BuffUIElement : MonoBehaviour
         {
             timeLeft -= Time.deltaTime;
             timeText.text = Mathf.CeilToInt(timeLeft).ToString();
+            cooldownMask.fillAmount = timeLeft / duration;
             yield return null;
         }
 
-        yield return StartCoroutine(FadeOut());
         onEnd?.Invoke();
-        Destroy(gameObject);
+        yield return StartCoroutine(FadeOut());
+        gameObject.SetActive(false);
     }
 
     private IEnumerator FadeOut()
